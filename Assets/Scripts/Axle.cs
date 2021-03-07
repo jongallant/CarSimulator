f36b6763b7ed274e0c9a11c6fa57aa74c58fa7a1
Guillaume Lecouteux <guillaume.lecouteux@greenergy.com>
2021-03-07 11:24:17 +0000

    Upgraded to 2019 LTS. Changed custom camera for cinemachine. Fixed the skidmark display glitches. Removed resource and find transform function and replaced with direct references.

--- Assets/Scripts/Axle.cs
+++ Assets/Scripts/Axle.cs
@@ -1,43 +1,35 @@
 ﻿using UnityEngine;
 
-public class Axle : MonoBehaviour {
+namespace CarSimulator2D
+{
+    public class Axle : MonoBehaviour
+    {
 
-	public float DistanceToCG { get; set; }
-	public float WeightRatio { get; set; } 
-	public float SlipAngle { get; set; }
-	public float FrictionForce {
-		get {
-			return (TireLeft.FrictionForce + TireRight.FrictionForce) / 2f;
-		}
-	}
-	public float AngularVelocity {
-		get {
-			return Mathf.Min (TireLeft.AngularVelocity + TireRight.AngularVelocity);
-		}
-	}
-	public float Torque {
-		get {
-			return (TireLeft.Torque + TireRight.Torque) / 2f;
-		}
-	}
+        public Tire TireLeft; // direct reference
+        public Tire TireRight; // direct reference
+        public Transform AxleTransform; // direct reference
 
-	public Tire TireLeft { get; private set; }
-	public Tire TireRight { get; private set; }
+        public float DistanceToCG { get; set; }
+        public float WeightRatio { get; set; }
+        public float SlipAngle { get; set; }
+        public float FrictionForce => (TireLeft.FrictionForce + TireRight.FrictionForce) / 2f;
+        public float AngularVelocity => Mathf.Min(TireLeft.AngularVelocity + TireRight.AngularVelocity);
+        public float Torque => (TireLeft.Torque + TireRight.Torque) / 2f;
 
-	void Awake() {
-		TireLeft = transform.Find ("TireLeft").GetComponent<Tire> ();
-		TireRight = transform.Find ("TireRight").GetComponent<Tire> ();
-	}
+        void OnEnable()
+        {
+        }
 
-	public void Init(Rigidbody2D rb, float wheelBase) {
+        public void Init(Rigidbody2D rb, float wheelBase)
+        {
 
-		// Weight distribution on each axle and tire
-		WeightRatio = DistanceToCG / wheelBase;
+            // Weight distribution on each axle and tire
+            WeightRatio = DistanceToCG / wheelBase;
 
-		// Calculate resting weight of each Tire
-		float weight = rb.mass * (WeightRatio * -Physics2D.gravity.y);
-		TireLeft.RestingWeight = weight;
-		TireRight.RestingWeight = weight;
-	}
-
+            // Calculate resting weight of each Tire
+            float weight = rb.mass * (WeightRatio * -Physics2D.gravity.y);
+            TireLeft.RestingWeight = weight;
+            TireRight.RestingWeight = weight;
+        }
+    }
 }
